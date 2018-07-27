2205e37c06aaa89e0cd70308c356d54b4bf47e0e
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-07-27 14:34:25 -0300

    Adding models on admin

--- kuehne/core/admin.py
+++ kuehne/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, City, Status, Shipment
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'updated', 'active')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'created', 'updated', 'active')
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('status', 'created', 'updated', 'active')
+
+
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ('object_number', 'status', 'actual_location', 'next_location', 'updated', 'active')
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Shipment, ShipmentAdmin)
