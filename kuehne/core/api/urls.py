b68fb1b78d0cb4c196d55522f96ff754ef5d2a83
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-07-28 18:18:52 -0300

    Improving url paterns

--- kuehne/core/api/urls.py
+++ kuehne/core/api/urls.py
@@ -4,12 +4,12 @@ from .views import CountryAPIView, CityAPIView, StatusAPIView, ShipmentAPIView
 
 
 urlpatterns = [
-   path('', ShipmentAPIView.as_view(), name='shipments'),
-   path('<int:object_number>/', ShipmentAPIView.as_view(), name='shipment'),
-   path('countries/', CountryAPIView.as_view(), name='countries'),
+   path('country/', CountryAPIView.as_view(), name='countries'),
    path('country/<int:pk>/', CountryAPIView.as_view(), name='country'),
-   path('cities/', CityAPIView.as_view(), name='cities'),
-   path('citiy/<int:pk>/', CityAPIView.as_view(), name='city'),
+   path('city/', CityAPIView.as_view(), name='cities'),
+   path('city/<int:pk>/', CityAPIView.as_view(), name='city'),
    path('status/', StatusAPIView.as_view(), name='status-list'),
-   path('status/<int:pk>/', StatusAPIView.as_view(), name='status')
+   path('status/<int:pk>/', StatusAPIView.as_view(), name='status'),
+   path('<str:object_number>/', ShipmentAPIView.as_view(), name='shipment'),
+   path('', ShipmentAPIView.as_view(), name='shipments')
 ]
