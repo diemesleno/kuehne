bedbcdcbc5a3c79532b9c541bfe59e8a103d5b81
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-07-28 18:19:13 -0300

    Using model mommy

--- kuehne/core/api/tests/test_views.py
+++ kuehne/core/api/tests/test_views.py
@@ -5,10 +5,18 @@ from django.utils.http import urlencode
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from model_mommy import mommy
+
 from kuehne.core.models import Country, City, Status, Shipment
 
 
 class CountryTest(APITestCase):
+    def setUp(self):
+        self.country_model = mommy.make('core.Country')
+    
+    def tearDown(self):
+        del self.country_model
+
     def create_country(self, name):
         url = reverse('countries')
         data = {"name": name}
@@ -21,9 +29,10 @@ class CountryTest(APITestCase):
         """
         new_country = 'Chile'
         response = self.create_country(new_country)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Country.objects.count(), 1)
-        self.assertEqual(Country.objects.get().name, new_country)
+        self.assertEqual(Country.objects.count(), 2)
+        self.assertEqual(Country.objects.filter(name=new_country).first().name, new_country)
 
     def test_create_duplicated_country(self):
         """ 
@@ -46,16 +55,14 @@ class CountryTest(APITestCase):
         url = reverse('countries')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['id'], 8)
+        self.assertEqual(response.data[0]['id'], 15)
         self.assertEqual(response.data[0]['name'], new_country)
     
     def test_update_a_country(self):
         """ 
         Ensure we can update a country
         """
-        new_country = 'Uruguay'
-        country = Country(name=new_country)
-        country.save()
+        country = self.country_model
 
         new_country_name = 'Uruguaiana'
         url = reverse('country', kwargs={'pk': country.pk})
@@ -63,16 +70,14 @@ class CountryTest(APITestCase):
         data = {"name": new_country_name}
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['id'], 9)
+        self.assertEqual(response.data['id'], 16)
         self.assertEqual(response.data['name'], new_country_name)
     
     def test_delete_a_country(self):
         """ 
         Ensure we can delete a country
         """
-        new_country = 'Guiana Francesa'
-        country = Country(name=new_country)
-        country.save()
+        country = self.country_model
 
         url = reverse('country', kwargs={'pk': country.pk})
         response = self.client.delete(url)
@@ -81,6 +86,14 @@ class CountryTest(APITestCase):
 
 
 class CityTest(APITestCase):
+    def setUp(self):
+        self.city_model = mommy.make('core.City')
+        self.country_model = mommy.make('core.Country')
+    
+    def tearDown(self):
+        del self.city_model
+        del self.country_model
+
     def create_city(self, name, country_id):
         url = reverse('cities')
         data = {"name": name, "country": country_id}
@@ -94,39 +107,34 @@ class CityTest(APITestCase):
         new_city = 'Cochabamba'
         country = Country(name='Bolivia')
         country.save()
+
         response = self.create_city(new_city, country.pk)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(City.objects.count(), 1)
-        self.assertEqual(City.objects.get().name, new_city)
+        self.assertEqual(City.objects.count(), 2)
+        self.assertEqual(City.objects.filter(name=new_city).first().name, new_city)
     
     def test_update_a_city(self):
         """ 
         Ensure we can update a city
         """
-        new_city = 'Uruguaiana'
-        country = Country(name='Russia')
-        country.save()
-        city = City(name=new_city, country=country)
-        city.save()
+        country = self.country_model
+        city = self.city_model
 
         new_city_name = 'Urugua'
         url = reverse('city', kwargs={'pk': city.pk})
-
         data = {"name": new_city_name, "country": country.pk}
+
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['id'], 3)
+        self.assertEqual(response.data['id'], 4)
         self.assertEqual(response.data['name'], new_city_name)
     
     def test_delete_a_city(self):
         """ 
         Ensure we can delete a city
         """
-        new_city = 'Guianópolis'
-        country = Country(name='Bitcoinland')
-        country.save()
-        city = City(name=new_city, country=country)
-        city.save()
+        country = self.country_model
+        city = self.city_model
 
         url = reverse('city', kwargs={'pk': city.pk})
         response = self.client.delete(url)
@@ -134,9 +142,15 @@ class CityTest(APITestCase):
 
 
 class StatusTest(APITestCase):
+    def setUp(self):
+        self.status_model = mommy.make('core.Status')
+    
+    def tearDown(self):
+        del self.status_model
+
     def create_status(self, status):
         url = reverse('status-list')
-        data = {"status": status}
+        data = {"name": status}
         response = self.client.post(url, data, format='json')
         return response
     
@@ -146,34 +160,31 @@ class StatusTest(APITestCase):
         """
         new_status = 'Lost'
         response = self.create_status(new_status)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Status.objects.count(), 1)
-        self.assertEqual(Status.objects.get().status, new_status)
+        self.assertEqual(Status.objects.count(), 2)
+        self.assertEqual(Status.objects.all()[0].name, new_status)
     
     def test_update_a_status(self):
         """ 
         Ensure we can update a status
         """
-        new_status = 'Leaving'
-        status_obj = Status(status=new_status)
-        status_obj.save()
+        status_obj = self.status_model
 
         new_status_name = 'Arrived'
         url = reverse('status', kwargs={'pk': status_obj.pk})
+        data = {"name": new_status_name}
 
-        data = {"status": new_status_name}
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['id'], 7)
-        self.assertEqual(response.data['status'], new_status_name)
+        self.assertEqual(response.data['id'], 12)
+        self.assertEqual(response.data['name'], new_status_name)
     
     def test_delete_a_status(self):
         """ 
         Ensure we can delete a status
         """
-        new_status = 'Missing'
-        status_obj = Status(status=new_status)
-        status_obj.save()
+        status_obj = self.status_model
 
         url = reverse('status', kwargs={'pk': status_obj.pk})
         response = self.client.delete(url)
@@ -181,6 +192,23 @@ class StatusTest(APITestCase):
 
 
 class ShipmentTest(APITestCase):
+    def setUp(self):
+        self.country_model = mommy.make('core.Country')
+        self.city_models = mommy.make('core.City', country=self.country_model, _quantity=2)
+        self.status_models = mommy.make('core.Status', _quantity=2)
+        self.shipment_model = mommy.make(
+            'core.Shipment', 
+            actual_location=self.city_models[0], 
+            next_location=self.city_models[1],
+            status=self.status_models[0]
+        )
+    
+    def tearDown(self):
+        del self.country_model
+        del self.city_models
+        del self.status_models
+        del self.shipment_model
+
     def create_shipment(self, object_number, actual_location, next_location, status):
         url = reverse('shipments')
         data = {
@@ -196,37 +224,27 @@ class ShipmentTest(APITestCase):
         """ 
         Ensure we can create a new Shipment and then retrieve it
         """
+        country = self.country_model
+        actual_location = self.city_models[0]
+        next_location = self.city_models[1]
+        status_obj = self.status_models[0]
+
         object_number = '123456789012'
-        country = Country(name='Brazil')
-        country.save()
-        actual_location = City(name='Ituiutaba', country=country)
-        actual_location.save()
-        next_location = City(name='Uberlândia', country=country)
-        next_location.save()
-        status_obj = Status(status='Preparing')
-        status_obj.save()
         response = self.create_shipment(object_number, actual_location.pk, next_location.pk, status_obj.pk)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Shipment.objects.count(), 1)
+        self.assertEqual(Shipment.objects.count(), 2)
     
     def test_update_a_shipment(self):
         """ 
         Ensure we can update a shipment
         """
-        country = Country(name='Senegal')
-        country.save()
-        actual_location = City(name='Roca', country=country)
-        actual_location.save()
-        next_location = City(name='Moka', country=country)
-        next_location.save()
-        status_obj = Status(status='In Transit')
-        status_obj.save()
-        shipment = Shipment(object_number='123409876544', actual_location=actual_location, next_location=next_location, status=status_obj)
-        shipment.save()
+        country = self.country_model
+        actual_location = self.city_models[0]
+        next_location = self.city_models[1]
+        status_obj = self.status_models[0]
+        shipment = self.shipment_model
+        new_status_obj = self.status_models[1]
 
-        new_status = 'Delivered'
-        new_status_obj = Status(status=new_status)
-        new_status_obj.save()
         url = reverse('shipment', kwargs={'object_number': shipment.object_number})
 
         data = {
@@ -236,22 +254,32 @@ class ShipmentTest(APITestCase):
         }
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['status_name'], new_status)
+        self.assertEqual(response.data['message'], 'Updated with success.')
+    
+    def test_update_a_shipment_with_wrong_object_number(self):
+        """ 
+        Ensure we receive the correct message if a wrong object_number is passed
+        """
+        url = reverse('shipment', kwargs={'object_number': "000000000000"})
+        data = {
+                "status": 1,
+                "actual_location": 1,
+                "next_location": 2
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEqual(response.data['message'], 'You must inform a valid object number')
+
     
     def test_delete_a_shipment(self):
         """ 
         Ensure we can delete a shipment
         """
-        country = Country(name='Italy')
-        country.save()
-        actual_location = City(name='Turim', country=country)
-        actual_location.save()
-        next_location = City(name='Rome', country=country)
-        next_location.save()
-        status_obj = Status(status='Going')
-        status_obj.save()
-        shipment = Shipment(object_number='123456789098', actual_location=actual_location, next_location=next_location, status=status_obj)
-        shipment.save()
+        country = self.country_model
+        actual_location = self.city_models[0]
+        next_location = self.city_models[1]
+        status_obj = self.status_models[1]
+        shipment = self.shipment_model
         
         url = reverse('shipment', kwargs={'object_number': shipment.object_number})
         response = self.client.delete(url)
